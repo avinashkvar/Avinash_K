05e3a4661279c16f90fac00255202535346d8754
avinashkvar <98077343+avinashkvar@users.noreply.github.com>
2022-03-26 16:08:04 +0530

    authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,23 @@
-const User = require('../models/user.model')
+const User = require('../models/user.model');
 
-const register = async(req, res) => {
+var jwt = require('jsonwebtoken');
+
+const newToken = (user) => {
+	 return jwt.sign({ user }, 'masai');
+};
+
+const register = async (req, res) => {
 	try {
-		let user = await User.findOne({email :req.body.email})
+		let user = await User.findOne({ email: req.body.email });
 
-        if(user){
-            return res.status(400).send('email is already exists')
-        }
-        user = await User.create(req.body)
+		if (user) {
+			return res.status(400).send('email is already exists');
+		}
+		user = await User.create(req.body);
 
-        return res.status(201).send(user)
+		const token = newToken(user);
+
+		return res.status(201).send({ user, token });
 	} catch (error) {
 		return res.status(500).send({ error: error.message });
 	}
